942f2dd72640545dbc39021b0d128ecdf5525ab9
임지욱 <topjlim@naver.com>
2022-04-19 10:27:26 +0900

    [0419] 할일 삭제 기능 구현

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,9 @@
           <TodoListItem
             v-for="(todoItem, index) in todoItems"
             :key="index"
+            :index="index"
             :todoItem="todoItem"
+            @remove="removeTodoItem"
           ></TodoListItem>
         </ul>
       </div>
@@ -64,6 +66,11 @@ export default Vue.extend({
     },
     fetchTodoItems() {
       this.todoItems = storage.fetch();
+    },
+    removeTodoItem(index: number) {
+      console.log("remove : " + index);
+      this.todoItems.splice(index, 1);
+      storage.save(this.todoItems);
     },
   },
   created() {
